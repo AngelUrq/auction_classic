e3fcb44baf3419d2afa6b1424711f048eae9bf9d
Ángel Z <angel4441916@gmail.com>
2023-11-07 11:37:40 +0100

    Update retrieve_data.sh

--- data/retrieve_data.sh
+++ data/retrieve_data.sh
@@ -4,8 +4,8 @@ echo "Retrieving token"
 
 client_key=""
 secret_key=""
-realm_id=""
-auction_house_id=""
+realm_id="4408"
+auction_house_id="2"
 
 token=`curl -u $client_key:$secret_key -d grant_type=client_credentials https://us.battle.net/oauth/token | jq -r '.access_token'`
 
