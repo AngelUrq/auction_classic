78d7a559d5f8871dde8858b2a0eaf84506357015
Mackl3mor3 <jgduihgfggl050@gmail.com>
2024-01-04 08:51:33 -0400

    fix queries

--- data/sql/auction_queries.sql
+++ data/sql/auction_queries.sql
@@ -1,6 +1,5 @@
 
 
-CREATE TEMPORARY TABLE AuctionsHours AS
 SELECT
     a.auction_id,
     a.bid / 10000 AS bid_in_gold,
@@ -14,16 +13,3 @@ SELECT
 FROM Auctions a
 JOIN ActionEvents ae ON a.auction_id = ae.auction_id
 GROUP BY a.auction_id
-
-SELECT
-    ah.auction_id,
-    ah.bid_in_gold,
-    ah.buyout_in_gold,
-    ah.unit_price,
-    ah.quantity,
-    ah.time_left,
-    ah.item_id,
-    ah.first_appearance_timestamp,
-    ah.hours_on_sale
-FROM AuctionsHours ah
-
