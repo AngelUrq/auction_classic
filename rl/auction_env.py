0e64ae5a0ac61132b180a58e11d9d6493794ab40
AngelUrq <angel4441916@gmail.com>
2024-05-21 18:03:17 +0200

    Add RL template

---
+++ rl/auction_env.py
@@ -0,0 +1,22 @@
+import gym
+from gym import spaces
+
+class AuctionEnv(gym.Env):
+
+    def __init__(self):
+        super(AuctionEnv, self).__init__()
+        
+        # Define the action and observation space
+        pass
+    
+    def step(self, action):
+        # Implement the step method
+        pass
+    
+    def reset(self):
+        # Implement the reset method
+        pass
+
+    def close(self):
+        # Implement the close method
+        pass
